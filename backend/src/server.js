2d86e2f6a7801e69d168b3112dc2f7cf7db92b20
SivaSpantag <siva.loganathan@spantagtech.com>
2025-12-23 16:49:48 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import authRoutes from "./routes/auth.route.js";
 import express from "express";
 import dotenv from "dotenv";
 import messageRoutes from "./routes/message.route.js";
-
+import { connectDB } from "./lib/db.js";
 
 
 
@@ -10,8 +10,14 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+app.use(express.json());
 
-app.listen(port || 3000, () => console.log(`Server running on port ${port}`));
+app.listen(port || 3000, () => {
+    console.log(`Server running on port ${port}`);
+    connectDB();
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+
